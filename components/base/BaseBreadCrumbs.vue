079866405b418ed40de8aebce2e0874de3889e4f
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-14 20:31:42 +0330

    feature: Add bread crumb component

---
+++ components/base/BaseBreadCrumbs.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  class: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {});
+
+const containerClass = computed(() => `breadcrumbs ${props.class}`);
+</script>
+<template>
+  <div :className="containerClass">
+    <ul v-if="$slots.default">
+      <slot />
+    </ul>
+  </div>
+</template>
