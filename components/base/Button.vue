c92dbbafe49ff3eebf90e192afe676dfa7ad48a2
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-14 21:08:10 +0330

    chore: make props optional

--- components/base/Button.vue
+++ components/base/Button.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 interface Props {
-  class: string;
-  type:
+  class?: string; // Optional class prop
+  type?:
     | "primary"
     | "secondary"
     | "neutral"
@@ -11,10 +11,10 @@ interface Props {
     | "info"
     | "success"
     | "warning"
-    | "error";
-  size: "sm" | "lg" | "md";
-  loading: false; // Not loading by default
-  disabled: boolean;
+    | "error"; // Button type
+  size?: "sm" | "lg" | "md"; // Button size
+  loading?: boolean; // Loading state
+  disabled?: boolean; // Disabled state
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -22,16 +22,25 @@ const props = withDefaults(defineProps<Props>(), {
   size: "md",
   disabled: false,
   loading: false,
+  class: "",
 });
 
-const computedClass = computed(() => `btn btn-${props.type} ${props.class}`);
+// Compute the button classes dynamically
+const computedClass = computed(() => {
+  const baseClass = `btn btn-${props.type} btn-${props.size}`;
+  return `${baseClass} ${props.class}`;
+});
 </script>
 
 <template>
-  <button :class="computedClass" :disabled="disabled">
+  <button
+    :class="computedClass"
+    :disabled="props.disabled || props.loading"
+    type="button"
+  >
     <!-- Loading state -->
     <span v-if="loading" class="loading loading-spinner"></span>
     <!-- Slot for button content -->
-    <slot> click </slot>
+    <slot>Click</slot>
   </button>
 </template>
