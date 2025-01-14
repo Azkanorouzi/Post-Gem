f8c229ad9220e41d4570aea4f83c85748454e85f
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-14 21:12:57 +0330

    chore: add nuxt icon package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2025-01-14",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxt/icon"],
   components: [
     "~/components", // Default behavior for all components
     {
@@ -22,4 +22,3 @@ export default defineNuxtConfig({
     },
   ],
 });
-
