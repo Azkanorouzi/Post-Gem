04958e9cdaf4cce92792797f436c3f145778f56b
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-14 20:29:33 +0330

    chore: changed a few small implementation detail of base card and base button, related to class name and customizability

---
+++ components/base/BaseButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Props {
+  class: string;
+  type:
+    | "primary"
+    | "secondary"
+    | "neutral"
+    | "accent"
+    | "ghost"
+    | "link"
+    | "info"
+    | "success"
+    | "warning"
+    | "error";
+  size: "sm" | "lg" | "md";
+  loading: false; // Not loading by default
+  disabled: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: "primary",
+  size: "md",
+  disabled: false,
+  loading: false,
+});
+
+const computedClass = computed(() => `btn btn-${props.type} ${props.class}`);
+</script>
+
+<template>
+  <button :class="computedClass" :disabled="disabled">
+    <!-- Loading state -->
+    <span v-if="loading" class="loading loading-spinner"></span>
+    <!-- Slot for button content -->
+    <slot> click </slot>
+  </button>
+</template>
--- components/base/BaseCard.vue
+++ components/base/BaseCard.vue
@@ -8,6 +8,9 @@ interface Props {
 
 const props = withDefaults(defineProps<Props>(), {
   type: "normal",
+  bodyClass: "",
+  titleClass: "",
+  actionsClass: "",
 });
 
 const cardClass = computed(() => `card w-96 card-${props.type}`);
@@ -17,11 +20,19 @@ const cardClass = computed(() => `card w-96 card-${props.type}`);
   <section :class="cardClass">
     <slot name="card-image" />
     <div class="card-body" :class="bodyClass">
-      <strong class="card-title" :class="titleClass">
+      <strong
+        class="card-title"
+        :class="titleClass"
+        v-if="$slots['card-title']"
+      >
         <slot name="title"> Title </slot>
       </strong>
       <slot />
-      <div class="card-actions" :class="actionsClass">
+      <div
+        class="card-actions"
+        :class="actionsClass"
+        v-if="$slots['card-actions']"
+      >
         <slot name="card-actions" />
       </div>
     </div>
