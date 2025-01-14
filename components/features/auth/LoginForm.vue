0c6c7c33dd542338f3c01931d9fd3763103a244b
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-14 21:09:54 +0330

    feature: add auth folder features and add the form

---
+++ components/features/auth/LoginForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <BaseCard class="bg-base-100 shadow-xl">
+    <template #card-title>Login</template>
+    <form @submit.prevent="$emit('submit-form')">
+      <!-- Email Field -->
+      <div class="form-control">
+        <label class="label" for="email">
+          <span class="label-text">Email</span>
+        </label>
+        <BaseInput
+          id="email"
+          v-model="email"
+          type="email"
+          placeholder="Enter your email"
+          class="input-bordered"
+          :required="true"
+        />
+      </div>
+
+      <!-- Password Field -->
+      <div class="form-control">
+        <label class="label" for="password">
+          <span class="label-text">Password</span>
+        </label>
+        <BaseInput
+          id="password"
+          v-model="password"
+          type="password"
+          placeholder="Enter your password"
+          class="input-bordered"
+          :required="true"
+        />
+      </div>
+
+      <!-- Submit Button -->
+      <div class="form-control mt-6">
+        <BaseButton class="btn-primary" type="primary" @click="handleLogin">
+          Login
+        </BaseButton>
+      </div>
+    </form>
+  </BaseCard>
+</template>
+
+<script setup>
+const email = ref("");
+const password = ref("");
+
+function handleLogin() {
+  console.log(email.value, password.value);
+}
+</script>
