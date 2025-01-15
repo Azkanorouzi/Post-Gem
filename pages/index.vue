dc2d7052d743090137de950eb0bd6737d27aebef
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-15 14:12:03 +0330

    feature: redirect from index.vue because supabase route guard couldn't understand the path

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
-<template>
-  <h1>Home</h1>
-</template>
+<script setup lang="ts">
+// Use the navigateTo function to redirect to /posts because supabase didn't have support for inclduing root directory for redirection
+navigateTo("/posts");
+</script>
