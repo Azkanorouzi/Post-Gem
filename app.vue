ae0f456d4a1c2d8c31982437d9a5869d241e747a
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-15 14:15:52 +0330

    feature: prepare tailwind.config.ts for theme change functionality

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import Navbar from "./components/features/navigation/Navbar.vue";
+</script>
+
 <template>
-  <main class="flex justify-center items-center min-h-screen bg-base-200">
+  <Navbar />
+  <main
+    class="flex justify-center items-center min-h-screen bg-base-200 flex-col"
+  >
     <NuxtPage />
   </main>
 </template>
