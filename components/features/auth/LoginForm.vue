c89049dc0c691900c51053491cda9f9bbeb3d7fa
Azka Norouzi <a.azkanorouzi@gmail.com>
2025-01-15 14:11:16 +0330

    refactor: Change implementaiton of auth forms

--- components/features/auth/LoginForm.vue
+++ components/features/auth/LoginForm.vue
@@ -1,56 +1,22 @@
-<template>
-  <AuthSuccessAlert>Success</AuthSuccessAlert>
-  <BaseCard class="bg-base-100 shadow-xl" title-class="text-primary">
-    <template #card-title>
-      <Icon name="line-md:login" />
-      Login
-    </template>
-    <form @submit.prevent="$emit('submit-form')">
-      <!-- Email Field -->
-      <div class="form-control">
-        <label class="label" for="email">
-          <span class="label-text">Email</span>
-        </label>
-        <BaseInput
-          id="email"
-          v-model="email"
-          type="email"
-          placeholder="Enter your email"
-          class="input-bordered"
-          :required="true"
-        />
-      </div>
-
-      <!-- Password Field -->
-      <div class="form-control">
-        <label class="label" for="password">
-          <span class="label-text">Password</span>
-        </label>
-        <BaseInput
-          id="password"
-          v-model="password"
-          type="password"
-          placeholder="Enter your password"
-          class="input-bordered"
-          :required="true"
-        />
-      </div>
-
-      <!-- Submit Button -->
-      <div class="form-control mt-6">
-        <BaseButton class="btn-primary" type="primary" @click="handleLogin">
-          Login
-        </BaseButton>
-      </div>
-    </form>
-  </BaseCard>
-</template>
-
 <script setup>
 const email = ref("");
 const password = ref("");
 
-function handleLogin() {
-  console.log(email.value, password.value);
-}
+function submitForm() {}
 </script>
+<template>
+  <BaseAuthForm
+    :is-loading="true"
+    v-model:email="email"
+    v-model:password="password"
+    @submit-form="submitForm"
+    spinner-class="text-primary"
+  >
+    <template #form-title>
+      <Icon name="line-md:login" class="text-primary" />
+      <span class="text-primary"> Login</span>
+    </template>
+
+    <template #submit-text> login up </template>
+  </BaseAuthForm>
+</template>
